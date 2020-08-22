e1c64561fcc304d4ad395de2ebe4c93f8b06181f
biscaboy <david@davidjdickinson.com>
2020-08-21 19:01:10 -0700

    Replaced cloud function code with code from Brad to connect from dev environment.

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,28 +1,42 @@
-exports.handler = function (event, context, callback) {
-    const secretContent = `
-        <h3>Welcome to the Secret Area</h3>
-        <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
-    `
-    let body
-
-    if (event.body) {
-        body = JSON.parse(event.body)
-    } else {
-        body = {}
+exports.handler = function(event, context, callback) {
+    const headers = {
+      "Access-Control-Allow-Origin" : "*",
+      "Access-Control-Allow-Headers" : "Content-Type"
+    }
+   
+    if (event.httpMethod !== "POST") {
+      return callback(null, {
+        statusCode: 200,
+        headers,
+        body: "This was not a POST request"
+      })
     }
     
-    if (body.password == "javascript") {
-        callback(null, {
-            statusCode: 200,
-            body: secretContent
-        }
-    )
+    const secretContent = `
+    <h3>Welcome To The Secret Area</h3>
+    <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
+    `
+    
+    let body
+   
+    if (event.body) {
+      body = JSON.parse(event.body)
     } else {
-        callback(null, {
-            statusCode: 401
-        }
-    )
+      body = {}
     }
-
-
-}
+   
+    if (body.password == "javascript") {
+      callback(null, {
+        statusCode: 200,
+        headers,
+        body: secretContent
+      })
+    } else {
+      callback(null, {
+        statusCode: 401,
+        headers
+      })
+    }
+   
+    
+  }
